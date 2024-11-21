b56563f284801d85ad39001a9d5f11630d539427
noiseorigin <shycheung@live.cn>
2024-11-22 01:37:51 +0800

    added ssr and deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: netlify(),
 });
